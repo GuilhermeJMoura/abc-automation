be5bb1652c8aa7d0b0f89f8fd1c1a5b5867efafd
Calebe Matias <calebe.matias@sou.inteli.edu.br>
2025-07-13 01:56:49 -0300

    feat: Implementa createWorkflow corretamente e cria rota de bypassCreateWorkflow para podermos testar a criação dos workflows diretamente da nossa API em NodeJS
    Introduces bypassCreateWorkflow and bypassActivateWorkflow controller methods and routes to allow direct workflow creation and activation. Updates n8nService to generate random workflow names and log API connections and workflow creation.

--- backend/services/n8nService.js
+++ backend/services/n8nService.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const { settings } = require('../app');
+const crypto = require('crypto');
 const api = axios.create({
   baseURL : process.env.N8N_API_URL,
   headers : {
@@ -6,13 +8,19 @@ const api = axios.create({
     'Content-Type'  : 'application/json',
   },
 });
+console.log(`🔗 Conectado ao N8N API em ${process.env.N8N_API_URL}`);
 
 /** Create workflow but keep inactive until validated */
 exports.createWorkflow = async (workflowJSON) => {
   const { data } = await api.post('/workflows', {
-    ...workflowJSON,
-    active: false,
+    connections: workflowJSON?.connections,
+    nodes: workflowJSON?.nodes,
+    name:  `Workflow ${crypto.randomBytes(4).toString('hex')}`, // random name
+    settings: {
+        "executionOrder": "v1"
+    }
   });
+  console.log('Workflow criado:', data);
   return data;
 };
 
